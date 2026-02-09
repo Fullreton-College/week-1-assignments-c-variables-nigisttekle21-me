8d01d99b402d3ef7d06fe252c5108cab9ba7dffb
nigist-02083466 <nigist.tekle21@gmail.com>
2026-02-08 22:55:51 -0500

    Update Assignment4.cpp

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -1,1 +1,19 @@
-/* code yourself */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int intProduct = a * b * c * d;
+    int intAverage = (a + b + c + d) / 4;
+
+    double dblProduct = static_cast<double>(a) * b * c * d;
+    double dblAverage = (static_cast<double>(a) + b + c + d) / 4;
+
+    cout << intProduct << " " << intAverage << endl;
+    cout << fixed << setprecision(3) << dblProduct << " " << dblAverage << endl;
+
+    return 0;
+}
